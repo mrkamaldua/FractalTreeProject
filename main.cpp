38b99baca98dc6bdc7440f3d78b9ada61b608913
Kamal Dua <mrkamaldua@gmail.com>
2018-11-05 15:15:09 +0530

    New Commit

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 int a=0;
 typedef struct
     {
-        double x;
-        double y;
+         double x;
+         double y;
     }Point;
 typedef struct
     {
@@ -17,13 +17,25 @@ typedef struct
            void drawTrunkLine(double length,Trunk*ptr,Trunk*leftTrunk,Trunk*rightTrunk)
          {
              int c=0,d=0,k=0;double sum=0;
-             double t;
-             double x0=ptr->Start->x;double y0=ptr->Start->y;
-          double x1=ptr->End->x;double y1=ptr->End->y;
-          double m=(y1-y0)/(x1-x0);
+              double t;
+              double x0=ptr->Start->x; double y0=ptr->Start->y;
+           double x1=ptr->End->x; double y1=ptr->End->y;
+           double m=(y1-y0)/(x1-x0);
           printf("\n%f",m);
-          double xt,yt;
-          if(!isinf(m))
+           double xt,yt;
+             if(isinf(m)||(m>58)||(m<-58))
+          {
+              xt=x1;
+              if(y1>=y0)
+              {
+              yt=y1+length;
+              }
+              else
+              {
+                yt=y1-length;
+              }
+          }
+           else if(!isinf(m))
           {
 
           xt=x1+length/sqrt(1+pow(m,2));
@@ -39,21 +51,10 @@ typedef struct
               yt=y1+(m*length/sqrt(1+pow(m,2)));
           }
           }
-          else
-          {
-              xt=x1;
-              if(y1>=y0)
-              {
-              yt=y1+length;
-              }
-              else
-              {
-                yt=y1-length;
-              }
-          }
+
          //rotation of xt,yt on both sides with respect to x1,y1
-         double T1[3][3]={cos(M_PI/6),-sin(M_PI/6),x1*(1-cos(M_PI/6))+y1*sin(M_PI/6),sin(M_PI/6),cos(M_PI/6),y1*(1-cos(M_PI/6))-x1*sin(M_PI/6),0,0,1};
-         double T2[3][1]={xt,yt,1};
+          double T1[3][3]={cos(M_PI/6),-sin(M_PI/6),x1*(1-cos(M_PI/6))+y1*sin(M_PI/6),sin(M_PI/6),cos(M_PI/6),y1*(1-cos(M_PI/6))-x1*sin(M_PI/6),0,0,1};
+          double T2[3][1]={xt,yt,1};
          double T3[3][1];
 
             for (c = 0; c < 3; c++) {
@@ -67,7 +68,7 @@ typedef struct
                 }
                 leftTrunk->End->x=T3[0][0];
                 leftTrunk->End->y=T3[1][0];
-                double T0[3][3]={cos(M_PI/-6),-sin(M_PI/-6),x1*(1-cos(M_PI/-6))+y1*sin(M_PI/-6),sin(M_PI/-6),cos(M_PI/-6),y1*(1-cos(M_PI/-6))-x1*sin(M_PI/-6),0,0,1};
+                 double T0[3][3]={cos(M_PI/-6),-sin(M_PI/-6),x1*(1-cos(M_PI/-6))+y1*sin(M_PI/-6),sin(M_PI/-6),cos(M_PI/-6),y1*(1-cos(M_PI/-6))-x1*sin(M_PI/-6),0,0,1};
               for (c = 0; c < 3; c++) {
              for (d = 0; d < 1; d++) {
              for (k = 0; k < 3; k++) {
@@ -79,9 +80,10 @@ typedef struct
                 }
                 rightTrunk->End->x=T3[0][0];
                 rightTrunk->End->y=T3[1][0];
-                if(length<3.0)
+                if(length<9.0)
                 {
-                    glColor3ub(30,147,45);
+                    //glColor3ub(123,104,238);
+                    glColor3ub(210,105,30);
                 }
                 else
                 {
@@ -99,9 +101,9 @@ typedef struct
          }
     void fractalTreeBranch(Trunk*ptr)
         {
-          double x1=ptr->Start->x;double y1=ptr->Start->y;
-          double x2=ptr->End->x;double y2=ptr->End->y;
-          double length=sqrt(pow(y2-y1,2)+pow(x2-x1,2))*0.6;
+           double x1=ptr->Start->x; double y1=ptr->Start->y;
+           double x2=ptr->End->x; double y2=ptr->End->y;
+           double length=sqrt(pow(y2-y1,2)+pow(x2-x1,2))*0.8;
           Trunk*leftTrunk=(Trunk*)malloc(sizeof(Trunk));
           Trunk*rightTrunk=(Trunk*)malloc(sizeof(Trunk));
           leftTrunk->Start=(Point*)malloc(sizeof(Point));leftTrunk->End=(Point*)malloc(sizeof(Point));
@@ -110,7 +112,7 @@ typedef struct
           rightTrunk->Start->x=x2;rightTrunk->Start->y=y2;
           //printf("%f %f %f %f",leftTrunk->End->x,leftTrunk->End->y,rightTrunk->End->x,rightTrunk->End->y);
           drawTrunkLine(length,ptr,leftTrunk,rightTrunk);
-           if(length>0.4)
+           if(length>10.0)
            {
 
           fractalTreeBranch(leftTrunk);
@@ -126,8 +128,9 @@ typedef struct
   void display()
     {
        glClear(GL_COLOR_BUFFER_BIT);
+       glColor3ub(210,105,30);
        glBegin(GL_LINES);
-       glVertex2f(350,50);
+       glVertex2f(350,125);
        glVertex2f(350,200);
        //glVertex2f(272,245);
        glEnd();
@@ -135,7 +138,7 @@ typedef struct
        Trunk*ptr=(Trunk*)malloc(sizeof(Trunk));
        ptr->Start=(Point*)malloc(sizeof(Point));
        ptr->End=(Point*)malloc(sizeof(Point));
-       ptr->Start->x=350;ptr->Start->y=50;
+       ptr->Start->x=350;ptr->Start->y=125;
        ptr->End->x=350;ptr->End->y=200;
        fractalTreeBranch(ptr);
     }
@@ -147,10 +150,25 @@ typedef struct
         gluOrtho2D(0.0f, 700.0f, 0.0f, 500);
         //glPointSize(5.0f) will work only with GL_POINTS not with GL_LINE
         //For gl_LINES you have to use gl_Line_Width
-        glLineWidth(100.3f);
+        glLineWidth(3.0f);
         //glEnable(0x8642);
         //glEnable(GL_LINE_SMOOTH);
          }
+     void myboard(unsigned char key ,int x,int y)
+     {
+         switch(key)
+         {
+         case 'z':
+             gluOrtho2D(50.0f, 700.0f, 50.0f, 500);
+            break;
+         default:
+            break;
+         }
+     }
+     void reshape(int w,int h)
+       {
+           glViewport(0,0,w,h);
+       }
   int main(int argc,char**argv)
      {
       glutInit(&argc,argv);
@@ -160,6 +178,7 @@ typedef struct
       glutCreateWindow("Fractal Tree Algorithm");
       init();
       glutDisplayFunc(display);
+      glutKeyboardFunc(myboard);
       //glutIdleFunc(display);//to display something over and over again
       glutMainLoop();
       return 0;
